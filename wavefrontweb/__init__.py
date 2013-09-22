0f3c450c5cdc7abe860ce19535ab0fdadcdc3dff
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-09-22 16:19:18 -0400

    Fix up things

--- wavefrontweb/__init__.py
+++ wavefrontweb/__init__.py
@@ -14,7 +14,9 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    config.add_renderer('.html', "pyramid.mako_templating.renderer_factory")
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
+
