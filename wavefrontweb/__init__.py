a403dcbbc8d75b72033b224a15ae6ba7a28b150a
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-09-28 23:20:47 +0000

    Added a smaller test page

--- wavefrontweb/__init__.py
+++ wavefrontweb/__init__.py
@@ -85,6 +85,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_renderer('.html', "pyramid.mako_templating.renderer_factory")
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('foo', '/foo')
     config.add_route('home', '/')
     config.add_route('socketio', 'socket.io/*remaining')
     config.scan()
--- wavefrontweb/views.py
+++ wavefrontweb/views.py
@@ -89,6 +89,15 @@ def my_view(request):
 #    return {'one': one, 'project': 'wavefront-web'}
     return {'project': 'wavefront-web'}
 
+@view_config(route_name='foo', renderer='templates/foo.html')
+def my_view2(request):
+#    try:
+#        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+#    except DBAPIError:
+#        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#    return {'one': one, 'project': 'wavefront-web'}
+    return {'project': 'wavefront-web'}
+
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
