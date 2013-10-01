9affd190ad8473d3b735f82d6111ac63d947bb02
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-10-01 01:52:36 +0000

    major refactor

--- wavefrontweb/static/js/main.js
+++ wavefrontweb/static/js/main.js
@@ -4,49 +4,22 @@
 angular.module('wavefrontweb', [])
     .controller('WavefrontWebController', function($scope, $socketio) {
         console.log('WavefrontWebController Initialized');
+/*
         $scope.redraw_sleep = 10000;
         $scope.init = function(cfg) {
             if(typeof cfg.redraw_sleep != 'undefined') {
                 $scope.redraw_sleep = cfg.redraw_sleep;
             }
         };
-        // $socketio.on('evnam', function(data) { ... } );
-        // $socketio.emit('evname', data);
-/*        $scope.wfdata = []; */
-        /*
+*/
+/*
         $scope.wfdata.push({timestamp: new Date(0), max: 10, min: 0});
         $scope.wfdata.push({timestamp: new Date(1000), max: 0, min: -10});
         $scope.wfdata.push({timestamp: new Date(2000), max: 10, min: -10});
         $scope.wfdata.push({timestamp: new Date(3000), max: 10, min: -10});
         $scope.wfdata.push({timestamp: new Date(4000), max: 10, min: -10});
         $scope.wfdata.push({timestamp: new Date(5000), max: 10, min: -10});
-        */
-/*        $socketio.on('update', function(data) {
-            for (binidx in data.update) {
-                bin = data.update[binidx]
-                console.log("Update: " + JSON.stringify(bin));
-                timestamp = new Date(bin.timestamp * 1000);
-                bin.timestamp = timestamp
-                $scope.wfdata.push(bin);
-            }
-        });
-        $socketio.emit('subscribe', ['TA_058A_BHN', 3600.0, 10.0]);
- */   
-
-        $scope.redraw = 0;
-        $scope.request_redraw = [0];
-
-        var redraw = function() {
-            $scope.$apply(function() {
-                if ($scope.request_redraw[0] > 0) {
-                    $scope.redraw += 1;
-                    $scope.redraw %= 2;
-                    $scope.request_redraw[0] = 0;
-                }
-            });
-        };
-
-        interval = setInterval(redraw, $scope.redraw_sleep);
+*/
     })
 
     .factory("$socketio", function($rootScope) {
@@ -80,7 +53,7 @@ angular.module('wavefrontweb', [])
             link: function($scope, $element, $attr) {
                 // https://github.com/mbostock/d3/blob/master/src/time/scale.js
                 var time_scaleLocalFormats = [
-                    [d3.time.format.utc("%Y"), true],
+                    [d3.time.format.utc("%Y"), function(d) { return true; }],
                     [d3.time.format.utc("%B"), function(d) { return d.getUTCMonth(); }],
                     [d3.time.format.utc("%b %d"), function(d) { return d.getUTCDate() != 1; }],
                     [d3.time.format.utc("%a %d"), function(d) { return d.getUTCDay() && d.getUTCDate() != 1; }],
@@ -98,10 +71,9 @@ angular.module('wavefrontweb', [])
                   };
                 }
 
-                /* console.log(JSON.stringify($attr)); */
                 var twin = parseFloat($attr.twin);
                 var tbin = parseFloat($attr.tbin);
-                $scope.data = [];
+                var data = [];
 
                 var _hash = function() {
                     return [$attr.srcname, String(twin), String(tbin)].join('_');
@@ -109,55 +81,60 @@ angular.module('wavefrontweb', [])
 
                 console.log(["Linking wfChart", _hash()].join(', '))
 
-                $socketio.on('error_' + _hash(), function(ename, emsg) {
-                    console.log(ename + ": " + emsg);
-                });
-
-                $socketio.on('update_' + _hash(), function(data) {
-                    console.log("Update " + _hash() + " len " + String(data.update.length))
-                    for (binidx in data.update) {
-                        bin = data.update[binidx]
-                        /* console.log("Update: " + JSON.stringify([_hash(), bin])); */
-                        timestamp = 
-                        bin1 = {
-                            timestamp: new Date((bin.timestamp - tbin / 2) * 1000),
-                            max: bin.max,
-                            min: bin.min,
-                            mean: bin.mean,
-                            nsamples: bin.nsamples
-                        };
-                        bin2 = {
-                            timestamp: new Date((bin.timestamp + tbin / 2) * 1000),
-                            max: bin.max,
-                            min: bin.min,
-                            mean: bin.mean,
-                            nsamples: bin.nsamples
-                        };
-                        $scope.data.push(bin1);
-                        $scope.data.push(bin2);
-                        var bincnt = Math.floor(twin / tbin);
-                        if ($scope.data.length > bincnt * 2) {
-                            $scope.data.splice(0,2)
-                        }
-                        $scope.request_redraw[0] = 1;
-                    }
-                });
-
-                $socketio.emit('subscribe', [$attr.srcname, twin, tbin, _hash()]);
-
                 var 
                     margin = {top: 16, right: 5, bottom: 3, left: 50},
                     width = parseInt($attr.width - margin.left - margin.right),
                     height = parseInt($attr.height - margin.top - margin.bottom);
 
-                // var parseDate = d3.time.format("%Y%m%d").parse;
-
                 var x = d3.time.scale.utc()
-                    //.range([0, width]);
                     .range([0, width]);
 
                 var y = d3.scale.linear()
                     .range([height, 0]);
+
+                var area = d3.svg.area()
+                    .x(function(d) { return x(d.timestamp); })
+                    .y0(function(d) { return y(d.min); })
+                    .y1(function(d) { return y(d.max); });
+                    
+                var line = d3.svg.line()
+                    .interpolate("basis")
+                    .x(function(d) { return x(d.timestamp); })
+                    .y(function(d) { return y(d.mean); });
+
+                var svg = d3.select($element[0]).append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom);
+
+                svg.append("defs")
+                    .append("clipPath")
+                            .attr("id", "clip")
+                        .append("rect")
+                            .attr("width", width)
+                            .attr("height", height);
+
+                svg.append('rect')
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .attr("x", 0)
+                    .attr("y", 0)
+                    .attr("class", "background");
+
+                function zoomed() {
+                    svg.select("path.area").attr("d", area);
+                    svg.select("path.line").attr("d", line);
+                    svg.select(".x.axis").call(xAxis);
+                    svg.select(".y.axis").call(yAxis);
+                }                    
+
+                var zoom = d3.behavior.zoom()
+                // how to limit range
+                /*    .scaleExtent([-2, 0]) */
+                    .on("zoom", zoomed);
+
+                var svg = svg.append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                    .call(zoom);
 
                 var xAxis = d3.svg.axis()
                     .scale(x)
@@ -170,58 +147,34 @@ angular.module('wavefrontweb', [])
                     .orient("left")
                     .tickSize(-width);
 
-                $scope.$watch('redraw', function(oldLen, newLen) {
-                    var data = $scope.data;
-                    if (!data.length) { return; }
+                var xAxisNode = svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + (height)  + ")")
+                    .call(xAxis);
 
-                    d3.select($element[0]).select('svg').remove();
+                var yAxisNode = svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis);
 
-                    var area = d3.svg.area()
-                        .x(function(d) { return x(d.timestamp); })
-                        .y0(function(d) { return y(d.min); })
-                        .y1(function(d) { return y(d.max); });
-                        
-                    var line = d3.svg.line()
-                        .x(function(d) { return x(d.timestamp); })
-                        .y(function(d) { return y(d.mean); });
+                var areaNode = svg
+                    .append('g')
+                            .attr("clip-path", "url(#clip)")
+                        .append("path")
+                            .datum(data)
+                            .attr("class", "area");
 
-                    var svg = d3.select($element[0]).append("svg")
-                        .attr("width", width + margin.left + margin.right)
-                        .attr("height", height + margin.top + margin.bottom);
+                var lineNode = svg
+                    .append('g')
+                            .attr("clip-path", "url(#clip)")
+                        .append("path")
+                            .datum(data)
+                            .attr("class", "line");
 
-                    var zoom = d3.behavior.zoom()
-                    /*    .x(x) */
-                    //    .y(y)
-                    /*    .scaleExtent([-2, 0]) */
-                        .on("zoom", zoomed);
+                function redraw() {
+                    // console.log("Redraw watch fired");
 
-                    function zoomed() {
-                        svg.select("path.area").attr("d", area);
-                        svg.select("path.line").attr("d", line);
-                        svg.select(".x.axis").call(xAxis);
-                        svg.select(".y.axis").call(yAxis);
-                    }                    
-
-                    svg.append('rect')
-                            .attr("width", width + margin.left + margin.right)
-                            .attr("height", height + margin.top + margin.bottom)
-                            .attr("x", 0)
-                            .attr("y", 0)
-                            .attr("class", "background");
-
-                    svg = svg.append("g")
-                            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-                            .call(zoom);
-
-                    // fix this; do we need to iterate data even?
-                    // does this modify in place?
-                    // data.forEach(function(d) {
-                    //     d.date = parseDate(d.date);
-                    //     d.low = +d.low;
-                    //     d.high = +d.high;
-                    // });
-
-                    x.domain(d3.extent(data, function(d) { return d.timestamp; }));
+                    var x_extent = d3.extent(data, function(d) { return d.timestamp; })
+                    x.domain(x_extent);
                     y.domain([d3.min(data, function(d) { return d.min; }),
                               d3.max(data, function(d) { return d.max; })]);
 
@@ -230,65 +183,63 @@ angular.module('wavefrontweb', [])
 
                     yAxis.tickValues(y.ticks(3));
 
-                    var height_f =  function(d) {
-                        var ydmax = y(d.max);
-                        var ydmin = y(d.min);
-                        var diff = ydmin - ydmax;
-                        return diff;
-                    };
+                    areaNode.attr("d", area).attr("transform", null);
+                    lineNode.attr("d", line).attr("transform", null);
 
-/*
-                    g.selectAll(".bar")
-                            .data(data)
-                        .enter().append("rect")
-                            .attr("class", "bar")
-                            .attr('width', function(d) { 
-                                    var r = (x(1000001000.0) - x(1000000000.0));
-                                    return r;
-                                    var r = width / data.length; 
-                                    return r; })
-                            .attr('height', height_f)
-                            .attr('x', function(d) { return x(d.timestamp); })
-                            .attr('y', function(d) { return y(d.max); })
-                        ;
-*/
+                    var tr = x(x_extent[0] - tbin * 1000);
+                    // console.log("Tr: " + tr);
+                    lineNode.transition()
+                        .ease("linear")
+                        .attr("transform", "translate(" + tr + ")");
+                    areaNode.transition()
+                        .ease("linear")
+                        .attr("transform", "translate(" + tr + ")");
 
-// These require data to be sorted by timestamp, but that's not how it arrives,
-// and we don't want to do that right now.
-// area could even simulate bars by adding more points
-// could use horizontal lines or some other symbol for mean instead of a
-// continuous line
-/**/
-                    svg.append("path")
-                        .datum(data)
-                        .attr("class", "area")
-                        .attr("d", area);
+                    xAxisNode.transition().ease('linear').call(xAxis);
+                    yAxisNode.transition().ease('linear').call(yAxis);
 
-                    svg.append("path")
-                        .datum(data)
-                        .attr("class", "line")
-                        .attr("d", line);
-/**/
+                }
 
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + (height)  + ")")
-/*                        .attr("transform", "translate(0,0)") */
-/*                        .attr("x", 0) */
-/*                        .attr("y", 0) */
-                        .call(xAxis);
+                function on_update(pkt) {
+                    if (pkt == undefined) {
+                        console.error("pkt undefined");
+                        return;
+                    }
+                    console.log("Update " + _hash() + " len " + String(pkt.update.length))
+                    for (binidx in pkt.update) {
+                        var bin = pkt.update[binidx]
+                        /* console.log("Update: " + JSON.stringify([_hash(), bin])); */
+                        var bin1 = {
+                            timestamp: new Date((bin.timestamp) * 1000),
+                            max: bin.max,
+                            min: bin.min,
+                            mean: bin.mean,
+                            nsamples: bin.nsamples
+                        };
+                        var bin2 = {
+                            timestamp: new Date((bin.timestamp + tbin) * 1000),
+                            max: bin.max,
+                            min: bin.min,
+                            mean: bin.mean,
+                            nsamples: bin.nsamples
+                        };
+                        data.push(bin1);
+                        data.push(bin2);
+                        var bincnt = Math.floor(twin / tbin);
+                        if (data.length > bincnt * 2) {
+                            data.splice(0,2)
+                        }
+                    }
+                    redraw();
+                }
 
-                    svg.append("g")
-                        .attr("class", "y axis")
-/*                        .attr("transform", "translate(" + (50)  + ",0)") */
-                        .call(yAxis);
-/*                        .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Amplitude"); */
+                $socketio.on('update_' + _hash(), on_update);
+
+                $socketio.on('error_' + _hash(), function(ename, emsg) {
+                    console.log(ename + ": " + emsg);
                 });
+
+                $socketio.emit('subscribe', [$attr.srcname, twin, tbin, _hash()]);
             }
         }
     })
